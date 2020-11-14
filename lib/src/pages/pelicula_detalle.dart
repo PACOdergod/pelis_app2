1b21f1a049a81a952fd0e4c28d17b6cb0e1c870d
PACOdergod <67033790+PACOdergod@users.noreply.github.com>
2020-11-13 18:19:39 -0600

    La pagina de detalles ya muestra la informacion principal de la pelicula, falta poner la seccion de actores

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -8,7 +8,20 @@ class PeliculaDetalle extends StatelessWidget {
 
     return Scaffold(
         body: CustomScrollView(
-      slivers: <Widget>[_crearAppbar(pelicula)],
+      slivers: <Widget>[
+        _crearAppbar(pelicula),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          SizedBox(
+            height: 10.0,
+          ),
+          _posterTitulo(context, pelicula),
+          _descripcion(pelicula),
+          _descripcion(pelicula),
+          _descripcion(pelicula),
+          _descripcion(pelicula),
+        ]))
+      ],
     ));
   }
 
@@ -32,6 +45,58 @@ class PeliculaDetalle extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 300),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image(
+              image: NetworkImage(pelicula.getPosterImg()),
+              height: 200.0,
+            ),
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Flexible(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                pelicula.originalTitle,
+                style: Theme.of(context).textTheme.headline6,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                pelicula.originalTitle,
+                style: Theme.of(context).textTheme.subtitle2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star_border),
+                  Text(pelicula.voteAverage.toString())
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+
+  Widget _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
