616d23f61d4ea02534d0689edca3225ca13293c7
PACOdergod <67033790+PACOdergod@users.noreply.github.com>
2020-11-15 16:14:16 -0600

    El modelo para manejar la respuesta de los actores esta terminado

---
+++ lib/src/models/actores_model.dart
@@ -0,0 +1,72 @@
+class Actores {
+  List<Actor> actores = new List();
+
+  Actores.fromJsonList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
+    jsonList.forEach((element) {
+      final actor = Actor.fromJsonMap(element);
+      actores.add(actor);
+    });
+  }
+}
+
+class Actor {
+  bool adult;
+  int gender;
+  int id;
+  String knownForDepartment;
+  String name;
+  String originalName;
+  double popularity;
+  String profilePath;
+  int castId;
+  String character;
+  String creditId;
+  int order;
+  String department;
+  String job;
+
+  Actor({
+    this.adult,
+    this.gender,
+    this.id,
+    this.knownForDepartment,
+    this.name,
+    this.originalName,
+    this.popularity,
+    this.profilePath,
+    this.castId,
+    this.character,
+    this.creditId,
+    this.order,
+    this.department,
+    this.job,
+  });
+
+  Actor.fromJsonMap(Map<String, dynamic> json) {
+    adult = json['adult'];
+    gender = json['gender'];
+    id = json['id'];
+    knownForDepartment = json['known_for_department'];
+    name = json['name'];
+    originalName = json['original_name'];
+    popularity = json['popularity'];
+    profilePath = json['profile_path'];
+    castId = json['cast_id'];
+    character = json['character'];
+    creditId = json['credit_id'];
+    order = json['order'];
+    department = json['department'];
+    job = json['job'];
+  }
+
+  getPosterImg() {
+    final _urlImg = 'https://image.tmdb.org/t/p/w500$profilePath';
+    //print(_urlImg);
+    if (profilePath == null) {
+      return 'https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101027/112815900-no-image-available-icon-flat-vector.jpg';
+    } else {
+      return _urlImg;
+    }
+  }
+}
