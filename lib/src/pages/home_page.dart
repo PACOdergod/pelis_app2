cab9fc98d4867f2d209de1a1ab830ce10249c88e
paco <paco.33353@gmail.com>
2021-07-30 13:42:30 -0500

    commit antes del realese

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -14,7 +14,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text('Peliculas'),
+          title: Text('PelisPaco'),
           backgroundColor: Colors.amber,
           actions: [
             IconButton(
